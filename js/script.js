ca136f98c53b2508fcfacb708018c751a1a81784
camomillacomposta <90345452+camomillacomposta@users.noreply.github.com>
2021-09-22 17:01:33 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,33 +1,38 @@
 (function () {
-  const second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24;
-
+  const second = 1000, minute = second * 60, hour = minute * 60, day = hour * 24;
   let birthday = "Sep 30, 2021 00:00:00",
-      countDown = new Date(birthday).getTime(),
-      x = setInterval(function() {    
-
-        let now = new Date().getTime(),
-            distance = countDown - now;
-
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
-
-        //do something later when date is reached
-        if (distance < 0) {
-          let headline = document.getElementById("headline"),
-              countdown = document.getElementById("countdown"),
-              content = document.getElementById("content");
-
-          headline.innerText = "It's my birthday!";
-          countdown.style.display = "none";
-          content.style.display = "block";
-
-          clearInterval(x);
-        }
-        //seconds
-      }, 0)
-  }());
+  countDown = new Date(birthday).getTime(),
+  x = setInterval(function() {
+    let now = new Date().getTime(),
+    distance = countDown - now;
+    
+	let progressCircleDays = document.getElementById("circle-days");
+	let progressCircleHours = document.getElementById("circle-hours");
+	let progressCircleMinutes = document.getElementById("circle-minutes");
+	let progressCircleSeconds = document.getElementById("circle-seconds");
+	
+    let radius = progressCircleDays.r.baseVal.value;
+    
+	let circumference = radius * 2 * Math.PI;
+    
+	progressCircleDays.style.strokeDasharray = circumference;
+	progressCircleHours.style.strokeDasharray = circumference;
+	progressCircleMinutes.style.strokeDasharray = circumference;
+	progressCircleSeconds.style.strokeDasharray = circumference;
+	
+	percentDays = Math.floor(distance / (day)) * 3.333333333333333;
+	percentHours = Math.floor((distance % (day)) / (hour)) * 4.166666666666667;
+	percentMinutes = Math.floor((distance % (hour)) / (minute)) * 1.666666666666667;
+	percentSeconds = Math.floor((distance % (minute)) / second) * 1.666666666666667;
+	
+	progressCircleDays.style.strokeDashoffset = circumference - (percentDays / 100) * circumference;
+	progressCircleHours.style.strokeDashoffset = circumference - (percentHours / 100) * circumference;
+	progressCircleMinutes.style.strokeDashoffset = circumference - (percentMinutes / 100) * circumference;
+	progressCircleSeconds.style.strokeDashoffset = circumference - (percentSeconds / 100) * circumference;
+	
+    document.getElementById("days").innerText = Math.floor(distance / (day)),
+    document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
+    document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+    document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+  }, 0)
+}());
